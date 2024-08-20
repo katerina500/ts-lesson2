5ae0466d615fa1bf173fd8a21bc6e41948e08377
katerina500 <ekaterina.paramore@yandex.ru>
2024-08-20 15:09:08 +0400

    ++

---
+++ scripts/script.ts
@@ -0,0 +1,23 @@
+ // @ts-nocheck
+
+ fetch("https://jsonplaceholder.typicode.com/users") 
+    
+    .then(response => response.json()) 
+    .then(json => { 
+   
+        let li = `<tr><th>ID</th><th>Name</th><th>Username</th><th>Phone</th><th>Email</th><th>Website</th></tr>`; 
+        
+        json.forEach(user => { 
+            li += `<tr> 
+                <td>${user.id} </td>
+                <td>${user.name} </td> 
+                <td>${user.username}</td>  
+                <td>${user.phone}</td> 
+                <td>${user.email}</td>
+                <td>${user.website}</td>           
+            </tr>`; 
+        }); 
+   
+    document.getElementById("users").innerHTML = li; 
+}); 
+
